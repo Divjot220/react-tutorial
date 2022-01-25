bf7edc9a516386b318176e1a713f1d7ec44f4a87
Divjot <dev@zyloto.com>
2022-01-25 22:32:56 +0530

    add function and class components

---
+++ src/components/Greet.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// function Greet() {
+//   return <h1>Hello Divjot</h1>
+// }
+
+// using es6 syntax, both the es6 and above ways gives the same output
+const Greet = () => <h1>Hello Divjot</h1>
+
+// export const Greet = () => <h1>Hello Divjot</h1>
+// the above way is named export
+
+// the below is default export 
+export default Greet;
